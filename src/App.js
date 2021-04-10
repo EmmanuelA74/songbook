beb9f5e91887d5874c54bc86651c20969cbd1c13
Josh Eslinger <jbeslinger@gmail.com>
2021-04-10 09:48:02 -0500

    Added pickhacks logo, made skeleton homepage in App class, changed size of homepage logo

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+      <img src={logo} className="App-logo" alt="logo"></img>
+      <h1>Welcome to SongBook</h1>
+      <h2>Type in your search terms:</h2>
+      <form>
+        <input type='text' id="song_name" name='song_name' placeholder='Song' />
+        <input type='text' id="artist_name" name='artist_name' placeholder='Artist' /><br />
+        <input type='submit' value='Search' />
+      </form>
+    </React.Fragment>
   );
 }
 
